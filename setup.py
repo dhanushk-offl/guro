abc0f37d72090e10a38a66548f70653b65743faa
dhanushk-offl <dhanushkandhan75@gmail.com>
2025-01-16 18:22:06 +0530

    Final Changes ✅

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     author="Dhanush Kandhan",
     author_email="dhanushkandhan75@gmail.com",
     description="A Simple System Monitoring & Benchmarking Toolkit",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/dhanushk-offl/guro",
     classifiers=[
